00c3f70a69b90ea303cb30b1f5daf57b46606885
박주형 <juhyung.park@mirero.co.kr>
2024-01-26 09:39:21 +0900

    feat: add normal wpf project

---
+++ WPF/WPF/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void OpenSubWindow_Click(object sender, RoutedEventArgs e)
+        {
+            SubWindow numberInputWindow = new SubWindow();
+            numberInputWindow.Owner = this;
+            numberInputWindow.ShowDialog();
+
+            bool isSave = numberInputWindow.DialogResult.HasValue && numberInputWindow.DialogResult.Value == true;
+            if (isSave)
+            {
+                int enteredNumber = numberInputWindow.EnteredNumber;
+                numberTextBlock.Text = enteredNumber.ToString();
+            }
+        }
+    }
+}
---
+++ WPF/WPF/SubWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace WPF
+{
+    public partial class SubWindow : Window
+    {
+        public int EnteredNumber { get; private set; }
+
+        public SubWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(numberTextBox.Text, out int enteredNumber))
+            {
+                EnteredNumber = enteredNumber;
+                DialogResult = true;
+            }
+            else
+                MessageBox.Show("유효한 숫자를 입력해주세요.");
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
