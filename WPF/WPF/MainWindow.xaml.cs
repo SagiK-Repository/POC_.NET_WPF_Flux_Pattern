7e80b52a9ac869bc2d6b2fd9e115bf26b8e011a5
박주형 <juhyung.park@mirero.co.kr>
2024-02-05 12:31:25 +0900

    feat : add WPF 기본 코드

---
+++ WPF/WPF/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace WPF
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private int count;
+        public int Count
+        {
+            get { return count; }
+            set
+            {
+                count = value;
+                OnPropertyChanged(nameof(Count));
+            }
+        }
+
+
+        public MainWindow()
+        {
+            Count = 0;
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Count++;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
