22dd6c9f43b846fae0a54d415438def19ad2cc07
박주형 <juhyung.park@mirero.co.kr>
2024-02-05 15:58:57 +0900

    feat : add WPF MVVM DevExpress

---
+++ WPF/WPF_MVVM_DevExpress/Model/Model.cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.Mvvm;
+
+namespace WPF_MVVM_DevExpress.Model;
+
+public class NumberModel : ViewModelBase
+{
+    public int Number
+    {
+        get { return GetProperty(() => Number); }
+        set { SetProperty(() => Number, value); }
+    }
+    public void IncrementNumber()
+    {
+        Number++;
+    }
+}
---
+++ WPF/WPF_MVVM_DevExpress/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using DevExpress.Mvvm.CodeGenerators;
+using WPF_MVVM_DevExpress.Model;
+
+namespace WPF_MVVM_DevExpress.ViewModels
+{
+    [GenerateViewModel]
+    public partial class MainViewModel
+    {
+        [GenerateProperty]
+        NumberModel _Model;
+
+        public MainViewModel()
+        {
+            _Model = new NumberModel();
+        }
+
+        [GenerateCommand]
+        void IncreseNumber() => _Model.IncrementNumber();
+    }
+}
