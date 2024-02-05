9deb24975df82b5f363234440d2c45882d19490a
박주형 <juhyung.park@mirero.co.kr>
2024-02-05 13:17:34 +0900

    feat : add WPF MVVM

---
+++ WPF/WPF_MVVM/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+
+namespace WPF_MVVM;
+
+class MainViewModel
+{
+    public NumberModel Model { get; set; }
+
+    public ICommand IncreaseCommand { get; private set; }
+
+    public MainViewModel()
+    {
+        Model = new NumberModel();
+        IncreaseCommand = new RelayCommand(IncreaseNumber);
+    }
+
+    private void IncreaseNumber()
+    {
+        Model.IncrementNumber();
+    }
+}
---
+++ WPF/WPF_MVVM/Model.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace WPF_MVVM;
+public class NumberModel : INotifyPropertyChanged
+{
+    private int number;
+
+    public int Number
+    {
+        get { return number; }
+        set
+        {
+            number = value;
+            OnPropertyChanged(nameof(Number));
+        }
+    }
+
+    public void IncrementNumber()
+    {
+        Number++;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
