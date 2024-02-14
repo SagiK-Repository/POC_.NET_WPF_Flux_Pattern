a04a0c2aab43224437596a19fe2bf8d2cc21f6c9
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-02-14 23:43:29 +0900

    feat : set viewmodel

--- WPF/Client.Business/View2ViewModel.cs
+++ WPF/Client.Business/View2ViewModel.cs
@@ -1,9 +1,22 @@
-﻿using DevExpress.Mvvm.CodeGenerators;
+﻿using Client.Domain.Count;
+using DevExpress.Mvvm.CodeGenerators;
+using Fluxor;
 
 namespace Client.Business
 {
     [GenerateViewModel]
     public partial class View2ViewModel
     {
+        public readonly IState<CountState> CountState;
+
+        [GenerateProperty]
+        int _Count;
+
+        public View2ViewModel(IState<CountState> counterState)
+        {
+            CountState = counterState;
+            Count = CountState.Value.Number;
+            CountState.StateChanged += (s, e) => Count = CountState.Value.Number;
+        }
     }
 }
