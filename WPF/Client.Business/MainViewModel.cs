4ce79266c85d002857331e2dd8e78aba4fc507f4
박주형 <juhyung.park@mirero.co.kr>
2024-02-14 16:45:30 +0900

    feat : MainViewModel 구성

--- WPF/Client.Business/MainViewModel.cs
+++ WPF/Client.Business/MainViewModel.cs
@@ -1,11 +1,49 @@
-﻿using DevExpress.Mvvm;
+﻿using Client.Domain.Count;
+using DevExpress.Mvvm;
 using DevExpress.Mvvm.CodeGenerators;
+using Fluxor;
 
 namespace Client.Business
 {
     [GenerateViewModel]
     public partial class MainViewModel : ViewModelBase
     {
+        private readonly IStore Store;
+        public new readonly IDispatcher Dispatcher;
+        public readonly IState<CountState> CountState;
+
+        public int Count
+        {
+            get { return GetProperty(() => Count); }
+            set { SetProperty(() => Count, value); }
+        }
+
+        public MainViewModel(IStore store, IDispatcher dispatcher, IState<CountState> counterState)
+        {
+            Store = store;
+            Dispatcher = dispatcher;
+            CountState = counterState;
+
+            Initialize();
+        }
+
+        #region Initialize
+        private void Initialize()
+        {
+            SetChangeEvent();
+            Store.InitializeAsync().Wait();
+        }
+
+        private void SetChangeEvent()
+        {
+            CountState.StateChanged += CounterState_StateChanged;
+        }
+        #endregion
+
+        private void CounterState_StateChanged(object? sender, EventArgs e)
+        {
+            Count = CountState.Value.Number;
+        }
 
     }
 }
