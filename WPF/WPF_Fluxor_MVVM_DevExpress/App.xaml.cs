26bf7ee416c8accd1e6f9aa415fc71a63738db29
박주형 <juhyung.park@mirero.co.kr>
2024-02-14 15:52:33 +0900

    feat : 기본적인 App.xaml.cs Service 정의

--- WPF/WPF_Fluxor_MVVM_DevExpress/App.xaml.cs
+++ WPF/WPF_Fluxor_MVVM_DevExpress/App.xaml.cs
@@ -1,8 +1,23 @@
-﻿using System.Windows;
+﻿using Fluxor;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Windows;
 
 namespace WPF_Fluxor_MVVM_DevExpress
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var services = new ServiceCollection();
+            services.AddScoped<MainWindow>();
+            services.AddFluxor(o => o
+                .ScanAssemblies(typeof(App).Assembly));
+
+            IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            var mainView = serviceProvider.GetRequiredService<MainWindow>();
+            mainView.Show();
+        }
     }
 }
